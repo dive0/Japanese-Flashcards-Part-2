c9d85769a3e97b9db38d42356b6871b144f7906f
dive0 <zhengshiwei5@gmail.com>
2023-03-11 16:39:10 -0500

    Create back and next button

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,11 +5,11 @@ import wordList from "./WordList.json";
 const Cards = (props) => {
   const { setCardCount } = props;
   const [wordIndex, setWordIndex] = useState(0);
-
   const [word, setWord] = useState(wordList[0].word);
   const [image, setImage] = useState("");
   setCardCount(wordList.length - 1);
   const [flipCard, setFlipCard] = useState(false);
+  const [prevWordIndex, setPrevWordIndex] = useState([]);
 
   useEffect(() => {
     setWord(wordList[wordIndex].word);
@@ -26,21 +26,39 @@ const Cards = (props) => {
     }
   };
 
+  // const nextWord = () => {
+  //   let randomNum = wordIndex;
+  //   while (randomNum === wordIndex) {
+  //     randomNum = Math.floor(Math.random() * (wordList.length - 1 - 1 + 1)) + 1;
+  //   }
+  //   setWordIndex(randomNum);
+  //   setFlipCard(false);
+  // };
+
   const nextWord = () => {
-    let randomNum = wordIndex;
-    while (randomNum === wordIndex) {
-      randomNum = Math.floor(Math.random() * (wordList.length - 1 - 1 + 1)) + 1;
+    if (prevWordIndex.length < wordList.length - 1) {
+      setWordIndex(wordIndex + 1);
+      setFlipCard(false);
+      setPrevWordIndex([...prevWordIndex, wordIndex]);
     }
-    setWordIndex(randomNum);
-    setFlipCard(false);
   };
+
+  const prevWord = () => {
+    if (prevWordIndex.length > 0) {
+      setWordIndex(prevWordIndex[prevWordIndex.length - 1]);
+      setFlipCard(false);
+      setPrevWordIndex(prevWordIndex.slice(0, prevWordIndex.length - 1));
+      console.log(prevWordIndex)
+    }
+  }
 
   return (
     <div className="Cards">
       <div className="word" onClick={flip}>
         <Phrase word={word} image={image} />
       </div>
-      <button onClick={nextWord}>Next</button>
+      <button onClick={prevWord} disabled={wordIndex === 0}>Back</button>
+      <button onClick={nextWord} disabled={wordIndex === wordList.length - 1}>Next</button>
     </div>
   );
 };
